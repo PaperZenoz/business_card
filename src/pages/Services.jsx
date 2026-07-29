7d759b7fbf7f340f24d71d03df546b9255790260
vik-mol <viktorvmoll@gmail.com>
2026-07-29 17:46:31 +0700

    update

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,7 +3,7 @@ const Services = () => {
     return (
         <div className="services">
             <h2>Товары и услуги</h2>
-            <img loading="lazy" src="images/services/1.webp" alt="Раздел в разработке" />
+            <img loading="lazy" src="images/services/1.webp" alt="Раздел в разработке"/>
             <div className="services__more">Раздел в разработке</div>
             <PageNavigation currentPage="/services" />
         </div>
